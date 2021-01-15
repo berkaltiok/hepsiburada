27dcd969b1f0b934b10cd9510f8dbf406effa2a5
Berk Altıok <berkpw@gmail.com>
2021-01-15 13:04:33 +0300

    feat: create header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <router-view/>
+  <div>
+    <Header/>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+  import Header from "@/components/Layout/Header";
+
+  export default {
+    components: {
+      Header
+    },
+  }
+</script>
 
 <style lang="scss">
   @import "./assets/scss/main";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </div>
 </template>
 
 <script>
