302a3ffc9e9d6be57d6cf68fb4aa06762d703c77
Berk Altıok <berkpw@gmail.com>
2021-01-20 21:06:28 +0300

    feat(actions): add notify badge

--- src/components/Layout/Header/Actions.vue
+++ src/components/Layout/Header/Actions.vue
@@ -2,7 +2,10 @@
   <div>
     <template v-for="item in links">
       <router-link :to="item.link" class="flex items-center px-4 rounded-full hover:bg-gray-100 h-12 transition-all">
-        <i class="icon" :class="'icon-' + item.icon"></i>
+        <span class="flex relative">
+          <span class="absolute -top-3.5 -right-3.5 p-0.5 h-6 w-6 font-semibold text-white rounded-full bg-orange-500 flex items-center justify-center text-2xs border-2 border-white" v-if="item.badge">{{ item.badge }}</span>
+          <i class="icon" :class="'icon-' + item.icon"></i>
+        </span>
         <span class="text-gray-700 text-sm font-semibold ml-3.5">{{ item.title }}</span>
       </router-link>
     </template>
@@ -17,6 +20,7 @@
           {
             icon: "cart",
             title: "Sepetim",
+            badge: 2,
             link: "/"
           },
           {
