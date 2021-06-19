15b6c63b13ef5ac762ede4ad51d3eb0d7757b7e0
Berk Altıok <berkpw@gmail.com>
2021-06-19 18:09:14 +0300

    feat(router): add scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,9 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  },
   routes
 })
 
